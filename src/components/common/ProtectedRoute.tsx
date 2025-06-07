a00fd7b7c86955d80c511047b4025dc338d7e928
miftawidaya <miftawidaya@gmail.com>
2025-06-07 17:32:33 +0700

    add protected route feature

---
+++ src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+// src/components/common/ProtectedRoute.tsx
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuth } from '@/utils/hooks/useAuth';
+
+const ProtectedRoute: React.FC = () => {
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    return <Navigate to='/auth/login' state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
